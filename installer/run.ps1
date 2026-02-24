ebab77d79dc678bdf7800693a31af1371d97b32d
Rod Christiansen <rchristiansen@ecuad.ca>
2026-02-23 21:55:48 -0800

    feat: Add --no-dialog option to BootstrapMate installer command

--- installer/run.ps1
+++ installer/run.ps1
@@ -4,4 +4,4 @@
 # Generated during build with URL: https://cimian.ecuad.ca/bootstrap/management.json
 
 Write-Host "Running BootstrapMate with configured URL..."
-& 'C:\Program Files\BootstrapMate\installapplications.exe' --url https://cimian.ecuad.ca/bootstrap/management.json
+& 'C:\Program Files\BootstrapMate\installapplications.exe' --url https://cimian.ecuad.ca/bootstrap/management.json --no-dialog
